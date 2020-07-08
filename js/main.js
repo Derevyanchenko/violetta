0974d1840233219aa0be6b2c406713a0d1129eb6
Derevyanchenko Danil <danilderevyanchenko@gmail.com>
2020-07-08 18:01:20 +0300

    Сделал демку банера

--- js/main.js
+++ js/main.js
@@ -8,6 +8,75 @@ $(document).ready(function() {
         $(this).toggleClass("header__toggle--active");
     });
 
+    // add input class in focus
+
+$(".md-form input, .md-form textarea").on("focus", function() {
+
+  if( this.value.trim() == '' ) {
+      $(this).next().addClass("active");
+      $(this).addClass("active");
+      $(this).siblings(".md-form__icon").addClass("active")
+  }
+
+});
+
+
+// remove input class in focus
+
+$(".md-form input, .md-form textarea").on("blur", function() {
+
+  if( this.value.trim() == '' ) {
+      $(this).next().removeClass("active");
+      $(this).removeClass("active");
+      $(this).siblings(".md-form__icon").removeClass("active")
+  }
+  
+  // form field validate 
+
+  if( this.value.trim() !== '' ) {
+      $(this).parent().addClass("validate_true");
+  } else {
+      $(this).parent().removeClass("validate_true");
+  }
+
+});
+
+// popup settings
+
+
+function show_popup()
+{
+  $(".overlay_popup").fadeIn(200);
+  $("body, html").css("overflow-y", "hidden");
+}
+
+function close_popup()
+{
+  $('.overlay_popup').fadeOut(200);
+  $("body, html").css("overflow-y", "");
+}
+
+
+$(".add-review-js").on("click", function(e) {
+  e.preventDefault();
+  show_popup();
+});
+
+$(".popup__close").on("click", function() {
+  close_popup();
+});
+
+
+$(document).mouseup(function (event) {
+  if ($(".popup").is(":visible")) {
+      var popup = $(".popup");
+      if (!popup.is(event.target) && popup.has(event.target).length === 0) {
+          close_popup();
+      }
+  }
+});
+
+
   // banner slider
 
   $('.banner__slider').slick({
@@ -46,3 +115,4 @@ $(document).ready(function() {
 
 
 });
+
